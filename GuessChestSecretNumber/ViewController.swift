06e7403c052503301f262541aabe6c3010e0f37b
rnooxx999 <rnooxx999@gmail.com>
2023-01-27 00:45:27 +0200

    Initial Commit

---
+++ GuessChestSecretNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessChestSecretNumber
+//
+//  Created by Ran Ra on 05/07/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
